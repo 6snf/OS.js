057f13a4994062d8f70c580430e765e1e8a3bf38
Anders Evenrud <andersevenrud@gmail.com>
2017-07-31 11:13:19 +0200

    Updated iframe-application example

--- src/templates/package/iframe-application/webpack.config.js
+++ src/templates/package/iframe-application/webpack.config.js
@@ -1,20 +1,11 @@
 const path = require('path');
 const osjs = require('osjs-build');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = new Promise((resolve, reject) => {
   const metadataFile = path.join(__dirname, 'metadata.json');
 
   osjs.webpack.createPackageConfiguration(metadataFile).then((result) => {
     const configuration = result.config;
-
-    const copy = [{
-      from: path.join(__dirname, 'data'),
-      to: 'data'
-    }];
-
-    configuration.plugins.push(new CopyWebpackPlugin(copy, {
-    }));
 
     resolve(configuration);
   }).catch(reject);
